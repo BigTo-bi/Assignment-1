292b873c91c2637d921f0eb5ef8696795377b6c5
BigTo-bi <sodeketobilobasuccess@gmail.com>
2025-09-20 21:56:42 +0100

    initial commit

---
+++ Assignment/modified.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    printf("This is line two.\n");
+    printf("And this is line three.\n");
+    return 0;
+}
---
+++ Assignment/name.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {           
+
+    printf("My name is Sodeke Tobiloba Success\n");
+    printf("I am 18 years old\n");
+
+    return 0;
+}
